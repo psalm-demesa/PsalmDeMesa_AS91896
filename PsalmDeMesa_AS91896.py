cb9d597210a1d0f97e6c76d354fd5574f5ccd631
Psalm de Mesa <demesap@wsc.school.nz>
2025-06-17 15:06:09 +1200

    update

--- PsalmDeMesa_AS91896.py
+++ PsalmDeMesa_AS91896.py
@@ -13,21 +13,52 @@ while True:
     continue_game = input("Would you like to continue the game? (yes/no)")
 
     if continue_game == "yes":
-        #Set up the number of lives
-        lives = 7
+    
 
         #Print statements that explains the rules of the game
+        print()
         print("Here are the rules of the game:")
         print()
         print("You will be playing a round of trying to guess the word. You will be given 7 chances to guess the word.")
+        print("You will type a letter that you think will be in the word. If the letter you've written is not in the word, you will be deducted 1 life.")
         print("We will reveal the word once you have guessed all the letters or if you have failed to guess the word.")
         print()
         print("Goodluck and Enjoy!")
 
+        #Set up the number of lives
+        lives = 7
+
+        #Setting up the randomiser and the game setup
+        word_list = ['tree','apple','banana','pepparoni','car','computer','chair','watch','milk','orange','circus','spaghetti']
+        chosen_word = random.choice(word_list)
+        def print_underscores(word):
+            for letter in word:
+                print(" _ ", end="") 
+            print()
+
+        #Game starts here
+        print("Ready?")
+        print()
+        print_underscores(chosen_word)
+        letter = input("Guess a Letter:")
+        letter.lower
+
+        #Decision making
+        if letter in chosen_word:
+            print("You have guessed a letter!")
+        if letter not in chosen_word:
+            print("Incorrect! You have {} lives remaining." .format(lives - 1))
+            lives -= 1
+
+    
 
 
 
 
+
+
+
+    #This is for when the user want to exit the game.
     if continue_game == "no":
         print("All good! Let's play again next time!")
     break
